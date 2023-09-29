fd6efaa52b478192df999884c6af5248c3c8ac15
Tim Delaney <delanetw@mail.uc.edu>
2023-09-29 14:08:18 -0400

    innitial commit

--- src/HighorLow.java
+++ src/HighorLow.java
@@ -1,9 +1,51 @@
 import java.util.Scanner;
+import java.util.Random;
 public class HighorLow
 {
     public static void main (String[] args)
     {
         boolean done = false;
         Scanner in = new Scanner(System.in);
+        Random generator = new Random();
+        int guess = 0;
+        int val = generator.nextInt(10) + 1;
+        String trash = "";
+
+        do
+        {
+
+            System.out.print("Choose a value from [1 - 10]: ");
+            if(in.hasNextInt())
+            {
+                guess = in.nextInt();
+                in.nextLine();
+                if(guess == val)
+                {
+                    System.out.println(guess + " is correct!");
+                    done = true;
+                }
+                else if(guess >= val)
+                {
+                    System.out.println(guess + " is too high.");
+                }
+                else if(guess <= val)
+                {
+                    System.out.println(guess + " is too low.");
+                }
+                else
+                {
+                    trash = in.nextLine();
+                    System.out.println("You said: " + trash + ".");
+                    System.out.println("Please enter a valid input.");
+                }
+            }
+            else
+            {
+                trash = in.nextLine();
+                System.out.println("You said: " + trash + ".");
+                System.out.println("Please enter a valid input.");
+            }
+
+        }while(!done);
     }
 }
