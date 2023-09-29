9cf864075ad29513ab9479e7a7fa1ec650bfa648
Tim Delaney <delanetw@mail.uc.edu>
2023-09-29 11:01:38 -0400

    innitial commit

--- src/RectangleInfo.java
+++ src/RectangleInfo.java
@@ -1,3 +1,51 @@
 import java.util.Scanner;
-public class RectangleInfo {
+public class RectangleInfo
+{
+    public static void main (String[] args)
+    {
+        boolean done = false;
+        Scanner in = new Scanner(System.in);
+        double length = 0.0;
+        double width = 0.0;
+        double diag = 0.0;
+        double area = 0.0;
+        double peri = 0.0;
+        String trash = "";
+
+        do
+        {
+
+            System.out.print("Enter the length: ");
+            if(in.hasNextDouble())
+            {
+                length = in.nextDouble();
+                in.nextLine();
+                System.out.println("Enter the width: ");
+                if(in.hasNextDouble())
+                {
+                    width = in.nextDouble();
+                    in.nextLine();
+                    area = length * width;
+                    peri = (length * 2) + (width * 2);
+                    diag = (length * length) + (width * width);
+                    System.out.println("Enter the price per gallon: ");
+
+
+                }
+                else
+                {
+                    trash = in.nextLine();
+                    System.out.println("You said: " + trash + ".");
+                    System.out.println("Please enter a valid input.");
+                }
+            }
+            else
+            {
+                trash = in.nextLine();
+                System.out.println("You said: " + trash + ".");
+                System.out.println("Please enter a valid input.");
+            }
+
+        }while(!done);
+    }
 }
