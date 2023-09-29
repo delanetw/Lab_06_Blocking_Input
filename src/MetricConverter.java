bc7b5aab07ce252e4693b8c988f446c37542ee8f
Tim Delaney <delanetw@mail.uc.edu>
2023-09-29 13:00:39 -0400

    innitial commit

--- src/MetricConverter.java
+++ src/MetricConverter.java
@@ -5,5 +5,10 @@ public class MetricConverter
     {
         boolean done = false;
         Scanner in = new Scanner(System.in);
+        double meter = 0.0;
+        double mile = 0.0;
+        double feet = 0.0;
+        double inch = 0.0;
+        String trash = "";
     }
 }
